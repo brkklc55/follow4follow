1ba3f6e7834542665ed5bc697bc211aaa5c8a5fd
Brkklc55 <brkklc5563@gmail.com>
2025-11-21 23:25:53 +0300

    Fix: Initialize Frame SDK and call ready()

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,17 +1,31 @@
 "use client";
 
 import { AuthKitProvider } from "@farcaster/auth-kit";
+import sdk from "@farcaster/frame-sdk";
+import { useEffect } from "react";
 
 const config = {
-    rpcUrl: "https://mainnet.optimism.io", // Or a free public RPC
-    domain: "example.com", // Should be real domain in prod
-    siweUri: "https://example.com/login",
+    rpcUrl: "https://mainnet.optimism.io",
+    domain: "follow4follow-seven.vercel.app",
+    siweUri: "https://follow4follow-seven.vercel.app/login",
 };
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const load = async () => {
+            sdk.actions.ready();
+        };
+        if (sdk && !isSDKLoaded) {
+            isSDKLoaded = true;
+            load();
+        }
+    }, []);
+
     return (
         <AuthKitProvider config={config}>
             {children}
         </AuthKitProvider>
     );
 }
+
+let isSDKLoaded = false;
